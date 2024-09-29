947b682e5ea9b3d5722470a0a63b69eb8d9d2455
Gavin Lynch <gavinlynch04@gmail.com>
2024-09-29 09:23:01 -0700

    better on mobile

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import './Styles/MenuBar.css';
 
+const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-const MenuBar = () => {
+    const toggleMenu = () => {
+        setMenuOpen(prevState => !prevState); // Correct toggle logic
+    };
+
     return (
         <nav className="navbar">
-            <div className="navbar-brand"></div>
-            <ul className="navbar-links">
+            <div className="hamburger" onClick={toggleMenu}>
+                {menuOpen ? '✖' : '☰'} {}
+            </div>
+            <ul className={`navbar-links ${menuOpen ? 'show' : ''}`}>
+                <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     );
 };
 
-export default MenuBar;
+export default Navbar;
