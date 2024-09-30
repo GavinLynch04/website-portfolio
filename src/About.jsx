747a0eff8bb00d7a502f709e96731c21d20e2b45
Gavin Lynch <gavinlynch04@gmail.com>
2024-09-29 21:02:41 -0700

    added projects

--- src/About.jsx
+++ src/About.jsx
@@ -18,13 +18,13 @@ const About = () => {
                 </div>
                 <div className="about_content">
                     <div className="about_body">
-                        <p className='aboutText'>With a passion for solving complex problems through AI and machine learning,
-                                I’m a third year Computer Science student at Cal Poly San Luis Obispo, specializing in
-                                AI and Machine Learning. I enjoy combining technical skills with creativity,
-                                especially when working with technologies like Python, TensorFlow, and React.
-                                I’m driven by curiosity and a desire to constantly learn, whether I’m building models to
-                                analyze genetic data or crafting intuitive user experiences. When I’m not coding,
-                                you can find me trail running, exploring new tech, or skiing.</p>
+                        <p className='aboutText'>Hi, I’m Gavin Lynch, a Computer Science student at Cal Poly,
+                            San Luis Obispo, specializing in Artificial Intelligence and Machine Learning.
+                            I’m passionate about building innovative solutions using technologies like Python,
+                            JavaScript, and the MERN stack, with a focus on machine learning research, software
+                            engineering, and genetics. When I'm not coding or exploring new tech, you'll likely find
+                            me trail running or skiing. I'm always excited to collaborate on impactful projects that
+                            bridge cutting-edge tech with real-world challenges.</p>
                     </div>
                     <div className="about_tech">
                         <button>
