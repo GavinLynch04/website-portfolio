51752e489d759786e94b46487e7d45633b64a2f8
Gavin Lynch <34323734+GavinLynch04@users.noreply.github.com>
2025-07-09 13:09:45 -0400

    Update Experience.jsx

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,22 +2,28 @@ import './Styles/Experience.css';
 
 const experiences = [
     {
-        title: 'Teaching Assistant',
+        title: 'Automation Engineer Intern',
+        company: 'Novo Nordisk',
+        date: 'May 2025 - Current',
+        description: 'Engineered a dynamic error reporting tool using Python and Pandas to automate error detection. Utilized SQL for data management and analysis in large-scale databases. Redesigned and deployed critical workflows in Syncade MES, integrating new data pipelines to enhance manufacturing efficiency.',
+    },
+    {
+        title: 'Machine Learning Researcher',
         company: 'Cal Poly, Computer Science Department',
-        date: 'Sep 2024 - Present',
-        description: 'Assisted students during lab sessions to improve their software engineering skills, hosted office hours for one-on-one tutoring, and collaborated with Professor Kubiak to grade and evaluate student work fairly.',
+        date: 'Nov 2024 - Current',
+        description: 'Refined machine learning models like Random Forest and AdaBoost to predict green card applicant wait times, increasing accuracy by over 10%. Collaborated with front-end developers to integrate these predictive models into a user-friendly interface.',
+    },
+    {
+        title: 'Teaching Assistant - Intro to Software Engineering',
+        company: 'Cal Poly, Computer Science Department',
+        date: 'Sept 2024 - Current',
+        description: 'Mentored over 90 students in software engineering fundamentals, including agile methodologies and Git/Github. Graded and provided detailed feedback on 200+ assignments and offered direct student support through office hours.',
     },
     {
         title: 'Machine Learning Researcher',
         company: 'Cal Poly, Animal Science Department',
-        date: 'Feb 2024 - Present',
-        description: 'Developed predictive models (Random Forest, BERT) achieving 85% accuracy in detecting mastitis in dairy cattle. Optimized genome processing, reducing file sizes by 50%. Identified key SNPs for disease prevention and breeding strategies. Presented research at the Animal Science Research Symposium, with plans for the PAG 32 Conference.',
-    },
-    {
-        title: 'Teaching Assistant',
-        company: 'CodePlatoon',
-        date: 'Feb 2024 - April 2024',
-        description: 'Led one-on-one consultations and group discussions, reinforcing key concepts and fostering a collaborative learning environment to enhance comprehension and mastery of course material.',
+        date: 'Feb 2024 - Current',
+        description: 'Developed predictive models (Random Forest, BERT) achieving 74% accuracy in detecting mastitis in dairy cattle. Implemented software to compress cattle genomes, reducing file size by over 50%. Identified key SNPs to enhance disease prevention and presented research at the Cal Poly Animal Science Research Symposium.',
     },
 ];
 
