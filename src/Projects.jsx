d7d77cb322b756284892216595a79c2e56fc81e1
Gavin Lynch <34323734+GavinLynch04@users.noreply.github.com>
2025-07-25 11:19:16 -0400

    Update Projects.jsx

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,6 +4,13 @@ import project1Image from './assets/project1.png';
 
 const projects = [
     {
+        title: 'Fast-Select',
+        description: 'A high-performance Python library powered by Numba and CUDA, offering accelerated algorithms for feature selection. Initially built to optimize the complete Relief family of algorithms, fast-select aims to expand and accelerate a wide range of feature selection methods to empower machine learning on large-scale datasets.',
+        screenshot: project1Image,
+        githubLink: 'https://github.com/GavinLynch04/FastSelect',
+        livePreview: 'https://pypi.org/project/fast-select/'
+    },
+    {
         title: 'QuickSplit',
         description: 'This project is designed to help split receipts among multiple different people, and make the task of divying up receipts much easier. Includes the ability to upload or capture a picture of a receipt, where it will be processed for easier understanding and spliting by you. QuickSplit implements VeryFi API technology for its OCR/Receipt processing.',
         screenshot: project1Image,
